19905eed3abbbe43a027ea67ca4b7a7fd46d551a
murgraydj <murgraydj@gmail.com>
2023-03-21 18:04:30 -0400

    Adds method to find factorial of a number

--- recursion_practice.rb
+++ recursion_practice.rb
@@ -1,3 +1,16 @@
 # Author: Murgray D. John
 # Date: 3/21/2023
 # Purpose: To revisit the concept of recursion using ruby
+# Questions sourced from https://www.codequizzes.com/computer-science/beginner/recursion
+
+# Define a recursive function that finds the factorial of a number.
+def fact(n)
+  if n == 1
+    return 1
+  else
+    return n * fact(n-1)
+  end
+end
+
+puts "The factorial of 4 is: #{fact(4)}"
+puts "The factorial of 10 is: #{fact(10)}"
