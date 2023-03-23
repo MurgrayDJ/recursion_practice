f8681eaf81e89c8623926bdd071c3dcf793ba2fe
murgraydj <murgraydj@gmail.com>
2023-03-23 11:14:26 -0400

    Adds iteration solution of Fibonacci method
    The method returns an array containing the first n numbers of Fibonacci.
    This is Part 1 of Assignment 1

--- recursion_project.rb
+++ recursion_project.rb
@@ -2,3 +2,35 @@
 # Date: 3/23/2023
 # Purpose: To complete the Odin Project's Recursion Project
 
+
+#Assignment 1 - Fibonacci
+
+#Part 1 - Regular iteration
+def fibs(num)
+  result = []
+  cur_num = 0
+  next_num = 1
+  
+  if num == 1
+    result << cur_num
+  elsif num == 2
+    result = [cur_num, next_num]
+  elsif num >= 3
+    result = [cur_num, next_num]
+    for i in 2...num do
+      sum = cur_num + next_num
+      cur_num = next_num
+      next_num = sum
+      result << sum
+    end
+  end
+  result
+end
+
+puts "fibs(0) is #{fibs(0)}"
+puts "fibs(1) is #{fibs(1)}"
+puts "fibs(2) is #{fibs(2)}"
+puts "fibs(3) is #{fibs(3)}"
+puts "fibs(8) is #{fibs(8)}"
+
+#Part 2 - Recursion
