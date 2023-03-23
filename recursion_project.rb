ddacbbc2f9a7316326f486e84964b44561932d70
murgraydj <murgraydj@gmail.com>
2023-03-23 16:06:34 -0400

    Adds merge sort method to recursion_project

--- recursion_project.rb
+++ recursion_project.rb
@@ -49,8 +49,38 @@ def fibs_rec(num, result=[])
   result
 end
 
-puts "fibs_rec(0) is #{fibs_rec(0)}"
-puts "fibs_rec(1) is #{fibs_rec(1)}"
-puts "fibs_rec(2) is #{fibs_rec(2)}"
-puts "fibs_rec(3) is #{fibs_rec(3)}"
-puts "fibs_rec(8) is #{fibs_rec(8)}"
+# puts "fibs_rec(0) is #{fibs_rec(0)}"
+# puts "fibs_rec(1) is #{fibs_rec(1)}"
+# puts "fibs_rec(2) is #{fibs_rec(2)}"
+# puts "fibs_rec(3) is #{fibs_rec(3)}"
+# puts "fibs_rec(8) is #{fibs_rec(8)}"
+
+
+# Merge Sort
+def merge_sort(arr)
+  new_arr = []
+  if arr.length == 1
+    arr
+  else
+    split_arr = arr.each_slice( (arr.size/2.0).round ).to_a
+    #p split_arr
+    left_half = merge_sort(split_arr[0])
+    right_half = merge_sort(split_arr[1])
+    until new_arr.length == arr.length
+      if left_half.empty?
+        new_arr += right_half
+      elsif right_half.empty?
+        new_arr += left_half
+      elsif left_half[0] < right_half[0]
+        new_arr << left_half.shift
+      else
+        new_arr << right_half.shift
+      end
+    end
+    new_arr
+  end
+end
+
+puts "[8,3,4,7,2,5,1,6] sorted is #{merge_sort([8,3,4,7,2,5,1,6])}"
+puts "[5,2,1,3,6,4] sorted is #{merge_sort([5,2,1,3,6,4])}"
+puts "[1,2,3,4] sorted is #{merge_sort([1,2,3,4])}"
